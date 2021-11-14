61d9d4e9636d6c28035255cacdb0bd25565c7b6b
Rafique Adam Cudjoe <rafiqueacudjoe@gmail.com>
2021-11-14 07:40:25 +0000

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div className="container mx-auto px-10 mb-8">
+      <Head>
+        <title>The TECH BLOG</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+    
+    </div>
+  )
+}
